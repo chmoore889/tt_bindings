fcfb3b60df654b6ea33f307cc713b207b8894f4a
Christopher Moore <51140505+chmoore889@users.noreply.github.com>
2024-05-07 11:02:25 -0400

    Fixed bindings

--- lib/src/measure.dart
+++ lib/src/measure.dart
@@ -6,57 +6,63 @@ import 'package:ffi/ffi.dart';
 import 'package:tt_bindings/src/bindings.dart' as bindings;
 import 'package:tt_bindings/src/measurement_params.dart';
 
-Stream<Map<int, int>> startMeasurement(MeasurementParams params) async* {
-  Future<void> isolateFunction(SendPort sendPort) async {
-    final ReceivePort receivePort = ReceivePort();
+Future<void> isolateFunction(SendPort sendPort) async {
+  final ReceivePort receivePort = ReceivePort();
 
-    Completer<MeasurementParams> completer = Completer();
-    bool shouldClose = false;
-    receivePort.listen((message) {
-      if(message is MeasurementParams) {
-        completer.complete(message);
-      } else if (message == null) {
-        shouldClose = true;
-        receivePort.close();
-      }
-    });
-    sendPort.send(receivePort.sendPort);
-
-    final Pointer<Void> nativeDevice = bindings.getTagger();
-
-    final Pointer<bindings.MeasurementParamsNative> nativeParams = malloc();
-    final Pointer<Int> nativeParamsDetArray = malloc(params.detectorChannels.length);
-
-    final MeasurementParams data = await completer.future;
-    data.copyToNative(nativeParams.ref, nativeParamsDetArray);
-    final Pointer<Void> nativeMeasurement = bindings.newMeasurement(nativeDevice, nativeParams.ref);
-    malloc.free(nativeParamsDetArray);
-    malloc.free(nativeParams);
-
-    final Map<int, int> tpsf = {};
-    while(!shouldClose) {
-      final Pointer<Pointer<bindings.MacroMicroNative>> macroMicroPointer = malloc();
-      final Pointer<Size> lengthPointer = malloc();
-      final int res = bindings.getData(nativeMeasurement, macroMicroPointer, lengthPointer);
-      if(res != 0) {
-        throw StateError('Error getting data');
-      }
-      final macroMicro = macroMicroPointer.value;
-      final length = lengthPointer.value;
-      malloc.free(macroMicroPointer);
-      malloc.free(lengthPointer);
-
-      for (int x = 0; x < length; x++) {
-        const int binSizePs = 50;
-        tpsf.update(macroMicro[x].microTime ~/ binSizePs, (value) => value + 1, ifAbsent: () => 1);
-      }
-      sendPort.send(tpsf);
-      malloc.free(macroMicro);
+  Completer<MeasurementParams> completer = Completer();
+  bool shouldClose = false;
+  receivePort.listen((message) {
+    if(message is MeasurementParams) {
+      completer.complete(message);
+    } else if (message == null) {
+      shouldClose = true;
+      receivePort.close();
     }
+  });
+  sendPort.send(receivePort.sendPort);
 
-    bindings.startMeasurement(nativeMeasurement);
+  final Pointer<Void> nativeDevice = bindings.getTagger();
+
+  final Pointer<bindings.MeasurementParamsNative> nativeParams = malloc();
+  final MeasurementParams params = await completer.future;
+  final Pointer<Int> nativeParamsDetArray = malloc(params.detectorChannels.length);
+
+  params.copyToNative(nativeParams.ref, nativeParamsDetArray);
+  final Pointer<Void> nativeMeasurement = bindings.newMeasurement(nativeDevice, nativeParams.ref);
+  malloc.free(nativeParamsDetArray);
+  malloc.free(nativeParams);
+
+  bindings.startMeasurement(nativeMeasurement);
+
+  final Map<int, int> tpsf = {};
+  while(!shouldClose) {
+    final Pointer<Pointer<bindings.MacroMicroNative>> macroMicroPointer = malloc();
+    final Pointer<Size> lengthPointer = malloc();
+    final int res = bindings.getData(nativeMeasurement, macroMicroPointer, lengthPointer);
+    if(res != 0) {
+      throw StateError('Error getting data');
+    }
+    final macroMicro = macroMicroPointer.value;
+    final length = lengthPointer.value;
+    malloc.free(macroMicroPointer);
+    malloc.free(lengthPointer);
+
+    for (int x = 0; x < length; x++) {
+      const int binSizePs = 50;
+      tpsf.update(macroMicro[x].microTime ~/ binSizePs, (value) => value + 1, ifAbsent: () => 1);
+    }
+    sendPort.send(tpsf);
+    malloc.free(macroMicro);
+
+    await Future.delayed(const Duration(milliseconds: 100));
   }
 
+  bindings.stopMeasurement(nativeMeasurement);
+  bindings.freeMeasurement(nativeMeasurement);
+  bindings.freeTagger(nativeDevice);
+}
+
+Stream<Map<int, int>> startMeasurement(MeasurementParams params) async* {
   final receivePort = ReceivePort();
   //Can't send sendport
   final isolate = await Isolate.spawn(isolateFunction, receivePort.sendPort);
